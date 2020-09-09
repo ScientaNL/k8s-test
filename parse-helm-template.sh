cb051da05c1966b4ee4bc41ab81b2a23584fcf33
S. de Rooij <s.derooij@scienta.nl>
2020-09-09 14:18:58 +0200

    pipe more into parse-helm-templates

--- parse-helm-template.sh
+++ parse-helm-template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 php /scienta-helm-versioning/bin/convert-versions.php convert -s$CHART_SUFFIX -c$COMMIT_SHA -- /helmsource /helmfiles $CHART_VERSION $SCIENTA_VERSION 1>/dev/null \
-&& helm template /helmfiles/scienta$CHART_SUFFIX/v$CHART_VERSION
+&& helm template /helmfiles/scienta$CHART_SUFFIX/v$CHART_VERSION "$@"
